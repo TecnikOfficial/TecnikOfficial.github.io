a794b86db25f32ac1744c405114877552112881c
tecnik <44696073+techgamers2@users.noreply.github.com>
2023-11-04 14:41:40 +0530

    Add files via upload

---
+++ wp-content/plugins/script.js
@@ -0,0 +1,10 @@
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+	alert ("Discord Username Copied");
+}
