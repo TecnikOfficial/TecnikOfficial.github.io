fe0e40700830570e1b48ff99717cfc9e17e6a4eb
tecnik <44696073+techgamers2@users.noreply.github.com>
2024-05-12 20:48:21 +0530

    Add files via upload

--- wp-content/plugins/script.js
+++ wp-content/plugins/script.js
@@ -1,10 +1,1 @@
-function setClipboard(value) {
-    var tempInput = document.createElement("input");
-    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-	alert ("Discord Username Copied");
-}
+function setClipboard(e){var o=document.createElement("input");o.style="position: absolute; left: -1000px; top: -1000px",o.value=e,document.body.appendChild(o),o.select(),document.execCommand("copy"),document.body.removeChild(o),alert("Discord Username Copied")}
