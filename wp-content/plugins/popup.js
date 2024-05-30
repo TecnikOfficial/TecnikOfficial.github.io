8fd17ddc8cc35bf23fa89f254a3f9a89c573129c
tecnik <44696073+techgamers2@users.noreply.github.com>
2024-05-30 12:36:44 +0530

    Update popup.js

--- wp-content/plugins/popup.js
+++ wp-content/plugins/popup.js
@@ -2,7 +2,7 @@ setTimeout(function() {
   document.getElementById('popup').style.display = 'block';
   
   document.getElementById('subscribeBtn').addEventListener('click', function() {
-    window.location.href = 'https://www.youtube.com/@tecnikofficial'; // Redirect to TecnikOfficial's YouTube channel
+    window.location.href = 'https://www.youtube.com/channel/UCXucwi4swKyTmCUB9RrFaQw?sub_confirmation=1'; // Redirect to TecnikOfficial's YouTube channel
   });
   
   document.getElementById('closeBtn').addEventListener('click', function() {
