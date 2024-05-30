dec6736db652e5080aa0571e45c5f4eee4910cb8
tecnik <44696073+techgamers2@users.noreply.github.com>
2024-05-30 19:50:37 +0530

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+const POPUP_TIMEOUT_MS = 10000;
+
+var timeout = setTimeout(function () {
+    document.getElementById('popup').style.display = 'block';
+
+    document.getElementById('subscribeBtn').addEventListener('click', function () {
+        window.location.href = 'https://www.youtube.com/channel/UCXucwi4swKyTmCUB9RrFaQw?sub_confirmation=1'; // Redirect to TecnikOfficial's YouTube channel
+    });
+
+    document.getElementById('closeBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+    });
+}, POPUP_TIMEOUT_MS);
