d78c8b6be832f868870a7ffe27ecbe7b1025ef8c
tecnik <44696073+TecnikOfficial@users.noreply.github.com>
2024-07-19 16:47:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/TecnikOfficial.github.io/',
   build: {
-    outDir: 'dist', // Output directory for the build
-    minify: 'terser', // Use Terser for minification
-    sourcemap: false, // Disable source maps in production build
-    // Add any other optimization options as needed
+    outDir: 'dist',
   },
 });
-
