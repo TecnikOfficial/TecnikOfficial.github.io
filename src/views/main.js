049adc731e23ee5473703b7d4eae40821ebe492e
tecnik <44696073+TecnikOfficial@users.noreply.github.com>
2025-04-20 19:57:04 +0530

    Update main.js

--- src/views/main.js
+++ src/views/main.js
@@ -1,19 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from "vue"
+import App from "./App.vue"
 
-const app = createApp(App);
-
-// Register the service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch(error => {
-                console.error('Service Worker registration failed:', error);
-            });
-    });
-}
-
-app.mount("#app");
+createApp(App).mount("#app")
