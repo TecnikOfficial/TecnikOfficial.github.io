de447e525c3387aba2dc6a723498e8beafc8b4f4
tecnik <44696073+TecnikOfficial@users.noreply.github.com>
2025-04-20 21:38:16 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from "vue"
-import App from "./App.vue"
+import { createApp } from 'vue';
+import App from './App.vue';
+import './styles/style.css'; // Import the global stylesheet
 
-createApp(App).mount("#app")
+createApp(App).mount('#app');
