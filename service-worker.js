3b54c8dc4ddf6b02469b9a25a84da7b706b268db
tecnik <44696073+TecnikOfficial@users.noreply.github.com>
2025-05-16 17:00:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-site-cache-v4.39-' + new Date().getTime(); // Append timestamp
+const CACHE_NAME = 'my-site-cache-v4.40-' + new Date().getTime(); // Append timestamp
 const urlsToCache = [
     '/',
     '/index.html',
